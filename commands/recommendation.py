685e30cb9143558af01a2890d1c456ce0e1344de
Jared Dantis <jareddantis@gmail.com>
2020-12-30 00:20:20 +0800

    Recommendation: Don't display embeds if there are no results

--- commands/recommendation.py
+++ commands/recommendation.py
@@ -85,7 +85,7 @@ class Recommendation(commands.Cog):
 
     async def __search(self, ctx, query):
         # Search for matches in Spotify
-        title = "Top 5 matching {0}s for '{1}'"
+        title = "Top 5 matching Spotify {0}s for '{1}'"
         description = "To recommend a result, type `rc!recselect {} <index>`."
         results = self.spotify.search(query, limit=5, type='artist,album,track')
         context = {
@@ -97,34 +97,43 @@ class Recommendation(commands.Cog):
         }
 
         # Show matching tracks
-        embed = Embed(title=title.format("track", query), description=description.format("track"), color=0x20ce09)
-        for item in enumerate(results['tracks']['items']):
-            ms = item[1]['duration_ms']
-            duration_sec = ceil((ms/1000)%60) if ms > 10000 else "0{}".format(ceil((ms/1000)%60))
-            duration = "{0}:{1}".format(floor((ms/(1000*60))%60), duration_sec)
-            field_name = "{0} {1} ({2})".format(self.__num_to_emoji(item[0] + 1), item[1]['name'], duration)
-            field_value = item[1]['artists'][0]['name']
-            embed.add_field(name=field_name, value=field_value, inline=False)
-            context["track"].append(item[1]['id'])
-        await ctx.send(embed=embed)
+        if len(results['tracks']['items']):
+            embed = Embed(title=title.format("track", query), description=description.format("track"), color=0x20ce09)
+            for item in enumerate(results['tracks']['items']):
+                ms = item[1]['duration_ms']
+                duration_sec = ceil((ms/1000)%60) if ms > 10000 else "0{}".format(ceil((ms/1000)%60))
+                duration = "{0}:{1}".format(floor((ms/(1000*60))%60), duration_sec)
+                field_name = "{0} {1} ({2})".format(self.__num_to_emoji(item[0] + 1), item[1]['name'], duration)
+                field_value = item[1]['artists'][0]['name']
+                embed.add_field(name=field_name, value=field_value, inline=False)
+                context["track"].append(item[1]['id'])
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send("No tracks matching '{}' found on Spotify.".format(query))
 
         # Show matching albums
-        embed = Embed(title=title.format("album", query), description=description.format("album"), color=0x20ce09)
-        for item in enumerate(results['albums']['items']):
-            field_name = "{0} {1}".format(self.__num_to_emoji(item[0] + 1), item[1]['name'])
-            field_value = "{0}, {1}".format(item[1]['artists'][0]['name'], item[1]['release_date'])
-            embed.add_field(name=field_name, value=field_value, inline=False)
-            context["album"].append(item[1]['id'])
-        await ctx.send(embed=embed)
+        if len(results['albums']['items']):
+            embed = Embed(title=title.format("album", query), description=description.format("album"), color=0x20ce09)
+            for item in enumerate(results['albums']['items']):
+                field_name = "{0} {1}".format(self.__num_to_emoji(item[0] + 1), item[1]['name'])
+                field_value = "{0}, {1}".format(item[1]['artists'][0]['name'], item[1]['release_date'])
+                embed.add_field(name=field_name, value=field_value, inline=False)
+                context["album"].append(item[1]['id'])
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send("No albums matching '{}' found on Spotify.".format(query))
 
         # Show matching artists
-        embed = Embed(title=title.format("artist", query), description=description.format("artist"), color=0x20ce09)
-        for item in enumerate(results['artists']['items']):
-            field_name = "{0} - {1}".format(self.__num_to_emoji(item[0] + 1), item[1]['name'])
-            field_value = "{} followers".format(item[1]['followers']['total'])
-            embed.add_field(name=field_name, value=field_value, inline=False)
-            context["artist"].append(item[1]['id'])
-        await ctx.send(embed=embed)
+        if len(results['artists']['items']):
+            embed = Embed(title=title.format("artist", query), description=description.format("artist"), color=0x20ce09)
+            for item in enumerate(results['artists']['items']):
+                field_name = "{0} - {1}".format(self.__num_to_emoji(item[0] + 1), item[1]['name'])
+                field_value = "{} followers".format(item[1]['followers']['total'])
+                embed.add_field(name=field_name, value=field_value, inline=False)
+                context["artist"].append(item[1]['id'])
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send("No artists matching '{}' found on Spotify.".format(query))
 
         # Save context for later
         await ctx.send("{0}: To recommend '{1}' as is, type only `rc!rec`.".format(ctx.author.mention, query))
