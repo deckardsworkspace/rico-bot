4b36da6c8062367f23eddfceb02044df23f730c0
Jared Dantis <jareddantis@gmail.com>
2020-12-29 16:21:54 +0800

    app.py: Implement proper Firebase svc auth, switch to decorator-style events

--- app.py
+++ app.py
@@ -1,28 +1,32 @@
-import discord
+from discord import Client, Activity, ActivityType
+from discord.ext.commands import Bot
 import os
 import pyrebase
 
 pyrebase_config = {
-    "apiKey": os.environ['FIREBASE_KEY'],
-    "authDomain": os.environ['FIREBASE_AUTHDOMAIN'],
-    "databaseURL": os.environ['FIREBASE_DBURL'],
-    "storageBucket": os.environ['FIREBASE_BUCKET']
+    'apiKey': os.environ['FIREBASE_KEY'],
+    'authDomain': os.environ['FIREBASE_AUTHDOMAIN'],
+    'databaseURL': os.environ['FIREBASE_DBURL'],
+    'storageBucket': os.environ['FIREBASE_BUCKET'],
+    'serviceAccount': {
+        'client_email': os.environ['FIREBASE_CLIENT_EMAIL'], 
+        'client_id': os.environ['FIREBASE_CLIENT_ID'],
+        'private_key': os.environ['FIREBASE_PRIVATE_KEY'].replace('\\n', '\n'),
+        'private_key_id': os.environ['FIREBASE_PRIVATE_KEY_ID'],
+        'type': 'service_account'
+    },
 }
 firebase = pyrebase.initialize_app(pyrebase_config)
 auth = firebase.auth()
-user = auth.sign_in_with_email_and_password(os.environ['FIREBASE_EMAIL'], os.environ['FIREBASE_PASSWORD'])
 db = firebase.database()
+client = Bot(command_prefix='rc!')
 
 
-class RicoClient(discord.Client):
-    async def on_ready(self):
-        results = db.child('test').push({'user': self.user})
-        print('Logged on as {0}!'.format(self.user))
-
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+@client.event
+async def on_ready():
+    db.child('test').push({'user': '{}'.format(client.user)})
+    client.change_presence(activity=Activity(name='you | rc!help', type=ActivityType.listening))
+    print('Logged on as {0}!'.format(client.user))
 
 
-client = RicoClient()
 client.run(os.environ['DISCORD_TOKEN'])
-
