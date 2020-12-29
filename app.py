f53c2967554727a7e06626750ba85e0e3e710fc8
Jared Dantis <jareddantis@gmail.com>
2020-12-29 20:22:56 +0800

    Separate config into its own module

--- app.py
+++ app.py
@@ -1,23 +1,11 @@
 from discord import Activity, ActivityType
 from discord.ext.commands import Bot
 from discord.ext.tasks import loop
-import os
+from config import get_var, get_pyrebase_config
 import pyrebase
+import commands
 
-pyrebase_config = {
-    'apiKey': os.environ['FIREBASE_KEY'],
-    'authDomain': os.environ['FIREBASE_AUTHDOMAIN'],
-    'databaseURL': os.environ['FIREBASE_DBURL'],
-    'storageBucket': os.environ['FIREBASE_BUCKET'],
-    'serviceAccount': {
-        'client_email': os.environ['FIREBASE_CLIENT_EMAIL'], 
-        'client_id': os.environ['FIREBASE_CLIENT_ID'],
-        'private_key': os.environ['FIREBASE_PRIVATE_KEY'].replace('\\n', '\n'),
-        'private_key_id': os.environ['FIREBASE_PRIVATE_KEY_ID'],
-        'type': 'service_account'
-    },
-}
-firebase = pyrebase.initialize_app(pyrebase_config)
+firebase = pyrebase.initialize_app(get_pyrebase_config())
 auth = firebase.auth()
 db = firebase.database()
 client = Bot(command_prefix='rc!')
@@ -28,9 +16,19 @@ async def on_ready():
     print('Logged on as {0}!'.format(client.user))
 
 
+@client.event
+async def on_message(message):
+    # Ignore messages sent by this bot
+    if message.author == client.user:
+        return
+
+    await client.process_commands(message)
+
+
 @loop(seconds=120)
 async def update_presence():
-    await client.change_presence(activity=Activity(name='you | rc!help', type=ActivityType.listening))
+    activity = Activity(name='you | rc!help', type=ActivityType.listening)
+    await client.change_presence(activity=activity)
 
 
 @update_presence.before_loop
@@ -39,4 +37,4 @@ async def update_presence_before():
 
 
 update_presence.start()
-client.run(os.environ['DISCORD_TOKEN'])
+client.run(get_var('DISCORD_TOKEN'))
