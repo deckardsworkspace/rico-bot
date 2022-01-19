266d5104154ef3547a279812f4d947bcf8d0b4cc
Jared Dantis <jareddantis@gmail.com>
2022-01-19 14:13:46 +0800

    views(np): Implement play/pause, skip, prev

--- views/now_playing.py
+++ views/now_playing.py
@@ -1,12 +1,14 @@
+from lavalink.models import BasePlayer
 from nextcord import ButtonStyle, Interaction
 from nextcord.ext.commands import Context
 from nextcord.ui import button, Button, View
 
 
 class NowPlayingView(View):
-    def __init__(self, ctx: Context):
+    def __init__(self, ctx: Context, player: BasePlayer):
         super().__init__()
         self.ctx = ctx
+        self.player = player
     
     @button(label='📃', style=ButtonStyle.grey)
     async def show_queue(self, _: Button, interaction: Interaction):
@@ -14,15 +16,21 @@ class NowPlayingView(View):
     
     @button(label='⏮️', style=ButtonStyle.grey)
     async def skip_backward(self, _: Button, interaction: Interaction):
-        pass
+        cmd = self.ctx.bot.get_command('previous')
+        return await self.ctx.invoke(cmd)
 
     @button(label='⏯️', style=ButtonStyle.blurple)
     async def toggle_pause(self, _: Button, interaction: Interaction):
-        pass
+        if self.player.paused:
+            cmd = self.ctx.bot.get_command('unpause')
+        else:
+            cmd = self.ctx.bot.get_command('pause')
+        return await self.ctx.invoke(cmd)
 
     @button(label='⏭️', style=ButtonStyle.grey)
     async def skip_forward(self, _: Button, interaction: Interaction):
-        pass
+        cmd = self.ctx.bot.get_command('skip')
+        return await self.ctx.invoke(cmd)
     
     @button(label='⏹️', style=ButtonStyle.red)
     async def stop(self, _: Button, interaction: Interaction):
