cbfcae9a34066fa160595774ae3d5de341534a08
Jared Dantis <jareddantis@gmail.com>
2022-08-05 15:34:12 +0800

    Remove Recommendation cog for now

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -1,27 +1,17 @@
 from .recommendation import Recommendation
 from .export import Export
 from .help import Help
-from .debug import Debug
 from .thread import ThreadManager
 from nextcord.ext.commands import Bot
-from util import get_var, get_pyrebase_config, Spotify
-import pyrebase
+from util import Spotify
 
 
 def setup(bot: Bot):
     # Instantiate Spotipy
     spotify = Spotify()
 
-    # Instantiate Pyrebase
-    firebase = pyrebase.initialize_app(get_pyrebase_config())
-    db = firebase.database()
-
     # Add cogs
-    bot.add_cog(Debug(bot))
     bot.add_cog(Help(bot))
-    bot.add_cog(Export(bot, db, spotify))
-    bot.add_cog(Recommendation(bot, db, spotify))
-
-    # Conditional cogs
-    if get_var('ENABLE_THREADMGR') == '1':
-        bot.add_cog(ThreadManager(bot, db))
+    bot.add_cog(Export(bot, spotify))
+    bot.add_cog(Recommendation(bot, spotify))
+    bot.add_cog(ThreadManager(bot))
