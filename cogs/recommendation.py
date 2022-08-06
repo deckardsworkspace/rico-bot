6471264391dd68088561c90ea33b2b91349d0828
Jared Dantis <jareddantis@gmail.com>
2022-08-06 19:55:44 +0800

    RecommendationCog: Implement `/list`

--- cogs/recommendation.py
+++ cogs/recommendation.py
@@ -1,9 +1,12 @@
-from dataclass.custom_embed import create_error_embed, create_success_embed
+from dataclass.custom_embed import CustomEmbed, create_error_embed, create_success_embed
+from dataclass.recommendation import Recommendation
 from nextcord import Guild, Interaction, slash_command, SlashOption, User
 from nextcord.ext import application_checks
 from nextcord.ext.commands import Cog
 from typing import Optional, TYPE_CHECKING
 from util.config import get_debug_guilds
+from util.list_util import list_chunks
+from util.paginator import Paginator
 from util.recommendation_parser import parse_recommendation
 if TYPE_CHECKING:
     from util.rico_bot import RicoBot
@@ -59,3 +62,56 @@ class RecommendationCog(Cog):
                 title='Recommendation added',
                 body=f'**{recommendation.title}** added to {recommendee.mention}\'s list.'
             ))
+
+    @slash_command(name='list', guild_ids=get_debug_guilds())
+    async def list(
+        self,
+        itx: Interaction,
+        list_server: Optional[bool] = SlashOption(
+            name='display_server_list',
+            description='Display the server\'s list instead of your own',
+            required=False,
+            default=False
+        )
+    ):
+        """
+        Display your list of recommendations.
+        """
+        await itx.response.defer(ephemeral=True)
+
+        # Get recommendations
+        recommendations = []
+        if list_server:
+            recommendations = self._bot.db.get_guild_recommendations(itx.guild_id)
+            if not recommendations:
+                return await itx.followup.send(embed=create_error_embed(body='There are no recommendations made to the server.'))
+        else:
+            recommendations = self._bot.db.get_user_recommendations(itx.user.id)
+            if not recommendations:
+                return await itx.followup.send(embed=create_error_embed(body='You have no recommendations.'))
+
+        # Create recommendation pages
+        pages = []
+        for chunk in list_chunks(recommendations, 5):
+            fields = []
+            item: Recommendation
+            for item in chunk:
+                fields.append((
+                    item.title,
+                    '\n'.join([x for x in [
+                        item.type,
+                        item.url,
+                        f'added by <@{item.recommender}> on <t:{int(item.timestamp.timestamp())}:f>'
+                    ] if x])
+                ))
+            
+            # Create embed
+            pages.append(CustomEmbed(
+                title=f'Recommendations for {itx.guild.name if list_server else itx.user.name}',
+                description=f'{len(recommendations)} total',
+                fields=fields
+            ).get())
+
+        # Run paginator
+        paginator = Paginator(itx)
+        await paginator.run(pages)
