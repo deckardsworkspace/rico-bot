22faaf69ad77d813ecab6f4b0c21359a4167d0c9
Jared Dantis <jareddantis@gmail.com>
2022-08-08 21:18:58 +0800

    dataclass: Rename Recommendation to Note

--- dataclass/note.py
+++ dataclass/note.py
@@ -1,14 +1,14 @@
 from dataclasses import dataclass
 from datetime import datetime
-from util.enums import RecommendationType
+from util.enums import NoteType
 
 
 @dataclass
-class Recommendation:
+class Note:
     id: str
     timestamp: datetime
-    recommendee: int
-    recommender: int
-    type: RecommendationType
+    sender: int
+    recipient: int
+    type: NoteType
     title: str
     url: str
